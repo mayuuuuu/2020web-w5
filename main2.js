08da6d1c6c7188fe71682e8bc61adeae776c767c
mayuuuuu <75403513+mayuuuuu@users.noreply.github.com>
2021-01-04 17:07:59 +0900

    Update main2.js

--- main2.js
+++ main2.js
@@ -20,7 +20,8 @@ function result() {
   let xItem = randomValueFromArray(insertX);
   let yItem = randomValueFromArray(insertY);
   let zItem = randomValueFromArray(insertZ);
-
+  
+  newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':inserty:',yItem);
   newStory = newStory.replace(':insertz:',zItem);
