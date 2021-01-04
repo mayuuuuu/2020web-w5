165b390081bc665fe1294ac562cdd1a9495bdd63
mayuuuuu <75403513+mayuuuuu@users.noreply.github.com>
2021-01-04 17:04:25 +0900

    Update main2.js

--- main2.js
+++ main2.js
@@ -25,6 +25,7 @@ function result() {
   newStory = newStory.replace(':inserty:',yItem);
   newStory = newStory.replace(':inserty:',yItem);
   newStory = newStory.replace(':insertz:',zItem);
+  newStory = newStory.replace(':inserty:',yItem);
 
   if(customName.value !== '') {
     const name = customName.value;
